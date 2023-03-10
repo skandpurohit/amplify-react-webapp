22cd1e8b808087bf6f2d862ec757d38f2ee4d846
Skand Purohit <skandpurohit@gmail.com>
2023-03-10 15:23:40 -0700

    updated landing page

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is landing page for testing application deployed using Tanzu Components !
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://tanzu.vmware.com/tanzu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explore VMware Tanzu
         </a>
       </header>
     </div>
